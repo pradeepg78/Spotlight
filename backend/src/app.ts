e2e46107b629c81a275bfe990838e84bc62fe5ab
pradeepg78 <pradg78@gmail.com>
2026-01-01 23:31:03 -0500

    added new event route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import { Express, Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import eventRoutes from './routes/events';
 
 const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Spotlight is running!'
+    })
+});
 
 // Health Check Endpoint
 app.get('/health', (req: Request, res: Response) => {
@@ -17,5 +24,7 @@ app.get('/health', (req: Request, res: Response) => {
         environment: process.env.NODE_ENV
     });
 });
+
+app.use('/api/events', eventRoutes);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,4 +11,5 @@ app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
     console.log(`📍 Environment: ${process.env.NODE_ENV}`);
     console.log(`🔗 Health check: http://localhost:${PORT}/health`);
+    console.log(`Base URL Check: http://localhost:${PORT}/`)
 });
